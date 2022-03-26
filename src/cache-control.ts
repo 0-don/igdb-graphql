4117a46cf8fe85a34a183e7e85202ab3279b9b6f
Don-Cryptus <don.cryptus@gmail.com>
2022-03-26 16:44:27 +0100

    initial commit

---
+++ src/cache-control.ts
@@ -0,0 +1,19 @@
+import { CacheHint } from 'apollo-server-types';
+import { Directive } from 'type-graphql';
+
+export function CacheControl({ maxAge, scope }: CacheHint) {
+  if (!maxAge && !scope) {
+    throw new Error('Missing maxAge or scope param for @CacheControl');
+  }
+
+  let sdl = '@cacheControl(';
+  if (maxAge) {
+    sdl += `maxAge: ${maxAge}`;
+  }
+  if (scope) {
+    sdl += ` scope: ${scope}`;
+  }
+  sdl += ')';
+
+  return Directive(sdl);
+}
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server';
+import responseCachePlugin from 'apollo-server-plugin-response-cache';
+import { buildSchema } from 'type-graphql';
+import { RecipeResolver } from './recipe-resolver';
+import { ApolloServerPluginCacheControl } from 'apollo-server-core';
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [RecipeResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginCacheControl({ defaultMaxAge: 5 }),
+      responseCachePlugin(),
+    ],
+  });
+
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+}
+
+bootstrap();
